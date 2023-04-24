a386dcfe178a2d61776c32a3a6a68dda2ce39577
Harshith D R <85189030+HarshithDR@users.noreply.github.com>
2023-04-24 17:31:20 +0530

    Update gmail.py
    removing personal details

--- backend/gmail.py
+++ backend/gmail.py
@@ -1,15 +1,15 @@
 import smtplib
 
-from_address = 'vindieselvd123123@gmail.com'
+from_address = '***********@gmail.com'
 
 def sendemail(to, content):
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
     server.starttls()
-    server.login(from_address, 'qwerty0987!@#$')
+    server.login(from_address, '***************')
     server.sendmail(from_address ,to, content)
     server.close()
 
 if __name__ == '__main__':
 
-    sendemail('amith13082001@gmail.com','hi')
+    sendemail('****************','hi')
